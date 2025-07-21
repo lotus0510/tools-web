1fdac072ce83962bcfd89dbd092edf4da261728d
lotus <lotus920510@gmail.com>
2025-07-22 02:41:27 +0800

    新增index.html文件，更新package.json中的主页链接，修正vite.config.ts中的基础路径，增加GitHub Actions部署配置中的内存限制。

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/toos-web/',
+  base: '/tools-web/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
