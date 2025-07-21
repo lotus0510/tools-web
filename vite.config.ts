cf197387bccdf030bb75efd97e0cae1f23858c21
lotus <lotus920510@gmail.com>
2025-07-22 03:24:42 +0800

    更新vite.config.ts中的基础路径为/tools-web/以适应新的项目结构。

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/tools-web/',  // ★★ 這裡要改成你的 repo 名稱
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
